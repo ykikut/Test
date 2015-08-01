5906cf88c65cf656f1df309a4d16bf20e4a006d1
kikuta <kikuta@kke.co.jp>
2015-08-01 12:29:46 +0900

    change

--- src/main/java/hoge/test0719/NewMain.java
+++ src/main/java/hoge/test0719/NewMain.java
@@ -15,7 +15,7 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello, world");
+        System.out.println("hello");
     }
     
 }
